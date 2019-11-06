2dbbb21c83287cb5bdbde2cdb2572b70bc718a71
markvaaz <marcosrodolfo32@gmail.com>
2019-11-06 10:45:24 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,20 @@
 var block = false;
 var load = true;
 var estilo = false;
+(function loadTheCorruption(){
+  let duration = 0.8;
+  let delay = 0.3;
+  let revealText = document.querySelector(".reveal");
+  let letters = revealText.textContent.split("");
+  revealText.textContent = "";
+  let middle = letters.filter(e => e !== " ").length / 2;
+  letters.forEach((letter, i) => {
+    let span = document.createElement("span");
+    span.textContent = letter;
+    span.style.animationDelay = `${delay + Math.abs(i - middle) * 0.1}s`;
+    revealText.append(span);
+  });
+})();
 function novaHora() {
   function pad(s) {
     return (s < 10) ? '0' + s : s;
