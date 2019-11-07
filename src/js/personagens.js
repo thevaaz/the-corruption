4e9911aa7a8f1787203a6210513a32d0b1c862e7
markvaaz <marcosrodolfo32@gmail.com>
2019-11-07 14:01:53 -0300

    Update personagens.js

--- src/js/personagens.js
+++ src/js/personagens.js
@@ -57,11 +57,11 @@ var personagem = {
         "noticia":"",
         "callback":[function(){localStorage.setItem("postagem3266820", `[{
   "conteudo":"Ansiosa para o jantar com Miguel 😍",
-  "horario":${novaHora()},
+  "horario":"14:08",
   "comentarios":"0"
 }]`);document.getElementById("notificacao").style.display="block"},function(){localStorage.setItem("postagem3266820", `[{
   "conteudo":"Ansiosa para o jantar com Miguel 😍",
-  "horario":${novaHora()},
+  "horario":"14:08",
   "comentarios":"0"
 }]`);document.getElementById("notificacao").style.display="block"}],
         "con":["",""]
@@ -74,7 +74,7 @@ var personagem = {
         "noticia":"",
         "callback":[function(){localStorage.setItem("postagem3266820", `[{
   "conteudo":"Ansiosa para o jantar com Miguel 😍",
-  "horario":${novaHora()},
+  "horario":"14:08",
   "comentarios":"0"
 }]`);document.getElementById("notificacao").style.display="block"},""],
         "con":["",""]
