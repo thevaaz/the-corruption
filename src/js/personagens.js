98f7e179aa223b64321532b86957270a2af6c56e
markvaaz <marcosrodolfo32@gmail.com>
2019-11-07 00:04:39 -0300

    Update personagens.js

--- src/js/personagens.js
+++ src/js/personagens.js
@@ -49,11 +49,11 @@ var personagem = {
         "nx":["6","6"],
         "noticia":"",
         "callback":[function(){localStorage.setItem("postagem3266820", `[{
-  "conteudo":"Alguma coisa",
+  "conteudo":"Ótimo jantar com Miguel hoje ;)",
   "horario":"17:42",
   "comentarios":"0"
 }]`);document.getElementById("notificacao").style.display="block"},function(){localStorage.setItem("postagem3266820", `[{
-  "conteudo":"Alguma coisa",
+  "conteudo":"Ótimo jantar com Miguel hoje ;)",
   "horario":"17:42",
   "comentarios":"0"
 }]`);document.getElementById("notificacao").style.display="block"}],
@@ -66,7 +66,7 @@ var personagem = {
         "nx":["6","6"],
         "noticia":"",
         "callback":[function(){localStorage.setItem("postagem3266820", `[{
-  "conteudo":"Alguma coisa",
+  "conteudo":"Ótimo jantar com Miguel hoje ;)",
   "horario":"17:42",
   "comentarios":"0"
 }]`);document.getElementById("notificacao").style.display="block"},""],
