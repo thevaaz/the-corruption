5b234442da314001af4c84ebd54ebf5afff78509
markvaaz <marcosrodolfo32@gmail.com>
2019-11-06 22:44:12 -0300

    Update switter.js

--- src/js/switter.js
+++ src/js/switter.js
@@ -1,6 +1,4 @@
-function personagemID(){
-  return document.body.id.split('id').join('');
-}
+let personagemID = document.body.id.split('id').join('');
 (function loading(){
   let fade = 9;
   let loadingTO = setTimeout(function(){
@@ -16,16 +14,16 @@ function personagemID(){
 (function carregarPerfil(){
   let nome = document.getElementById('nome');
   let foto = document.getElementById('foto');
-  foto.backgroundImage = `url('${personagem[personagemID()].foto'})`;
-  nome.innerHTML = personagem[personagemID()].nome;
+  foto.backgroundImage = `url('${personagem[personagemID].foto'})`;
+  nome.innerHTML = personagem[personagemID].nome;
 })();
 (function carregarPostagens(){
   let linhaDoTempo = document.getElementById('linha-do-tempo');
-  let postagem = JSON.parse(localStorage.getItem('postagem'+personagemID()));
+  let postagem = JSON.parse(localStorage.getItem('postagem'+personagemID));
   linhaDoTempo.innerHTML = `<div class="post">
     <div class="post-top">
       <div class="foto"></div>
-      <div class="nome">${personagem[personagemID()].nome}</div>
+      <div class="nome">${personagem[personagemID].nome}</div>
       <div class="data">${postagem[0]["horario"]}</div>
     </div>
     <div class="post-bottom">
