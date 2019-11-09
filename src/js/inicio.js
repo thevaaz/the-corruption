33b31cc8ca5b80c8c7d5b536d40f38ee6e3bd2d5
markvaaz <marcosrodolfo32@gmail.com>
2019-11-08 22:15:37 -0300

    Update inicio.js

--- src/js/inicio.js
+++ src/js/inicio.js
@@ -9,3 +9,32 @@ for(let i = 0; i < apps.length; i++){
     setTimeout(function(){exp.style.display='none'},2000)
   })
 }
+if(document.querySelector(".reveal")){
+  (function loadTheCorruption(){
+    let duration = 0.8;
+    let delay = 0.3;
+    let revealText = document.querySelector(".reveal");
+    let letters = revealText.textContent.split("");
+    let tcLoading = document.getElementsByClassName("tc-loading")[0];
+    revealText.textContent = "";
+    let middle = letters.filter(e => e !== " ").length / 2;
+    let tcOpacity = 9;
+    letters.forEach((letter, i) => {
+      let span = document.createElement("span");
+      span.textContent = letter;
+      span.style.animationDelay = `${delay + Math.abs(i - middle) * 0.1}s`;
+      revealText.append(span);
+    });
+    setTimeout(function(){
+      let intOp = setInterval(function(){
+        tcLoading.style.opacity = '0.'+tcOpacity;
+        if(tcOpacity > 0){
+          tcOpacity--;
+        }else{
+          tcLoading.style.display = 'none';
+          clearInterval(intOp);
+        }
+      }, 30);
+    }, 3500);
+  })();
+}
