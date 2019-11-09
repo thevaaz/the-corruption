0ca343db96c5d9c6e955a63b235ed2f1ab27079f
markvaaz <marcosrodolfo32@gmail.com>
2019-11-08 22:15:22 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,35 +1,6 @@
 var block = false;
 var load = true;
 var estilo = false;
-if(document.querySelector(".reveal")){
-  (function loadTheCorruption(){
-    let duration = 0.8;
-    let delay = 0.3;
-    let revealText = document.querySelector(".reveal");
-    let letters = revealText.textContent.split("");
-    let tcLoading = document.getElementsByClassName("tc-loading")[0];
-    revealText.textContent = "";
-    let middle = letters.filter(e => e !== " ").length / 2;
-    let tcOpacity = 9;
-    letters.forEach((letter, i) => {
-      let span = document.createElement("span");
-      span.textContent = letter;
-      span.style.animationDelay = `${delay + Math.abs(i - middle) * 0.1}s`;
-      revealText.append(span);
-    });
-    setTimeout(function(){
-      let intOp = setInterval(function(){
-        tcLoading.style.opacity = '0.'+tcOpacity;
-        if(tcOpacity > 0){
-          tcOpacity--;
-        }else{
-          tcLoading.style.display = 'none';
-          clearInterval(intOp);
-        }
-      }, 30);
-    }, 3500);
-  })();
-}
 /*function novaHora() {
   function pad(s) {
     return (s < 10) ? '0' + s : s;
