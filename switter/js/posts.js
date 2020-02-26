1b0e9503037c622a4b5a3484f9f0329f543399c6
thevaaz <markvaaz@gmail.com>
2020-02-26 15:53:49 +0000

    0

--- switter/js/posts.js
+++ switter/js/posts.js
@@ -1,9 +1,15 @@
 function loadPosts(){
   personagem[personagemId].switter.posts.forEach(post => {
+    let data = new Date(Number(post.data))
+    let dia = data.getDate() < 10 ? "0" + data.getDate() : data.getDate();
+    let mes = (data.getMonth() + 1) < 10 ? "0" + (data.getMonth() + 1) : (data.getMonth() + 1);
+    let ano = data.getFullYear();
+
     document.getElementsByClassName("feed")[0].appendChild(create("div", {className:"post", appendChild:[
       create("div", {className:"profile-info", appendChild:[
         create("div", {className:"profile-picture", style:`background-image:url("${personagem[personagemId].foto}");`}),
-        create("div", {className:"profile-name", innerHTML:personagem[personagemId].nome}),
+        create("div", {className:"post-info", appendChild:[create("div", {className:"profile-name", innerHTML:personagem[personagemId].nome}),
+        create("div", {className:"post-date", innerHTML:`${dia}/${mes}/${ano}`})]})
       ]}),
       create("div", {className:"post-content", innerHTML:post.conteudo})
     ]}))
